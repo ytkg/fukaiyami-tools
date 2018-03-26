f5a771c2514af48b4f2a5e36225ebf283a919de4
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2018-03-26 23:50:44 +0900

    [clean] refactoring phase 1

--- js/script.js
+++ js/script.js
@@ -7,33 +7,21 @@ $(function() {
     if (current == -1) {
       console.log("non")
     } else if (current == 0) {
-      $(".in_display").addClass("non_display");
-      $("#a_image").removeClass("non_display");
       $(".in_display").removeClass("in_display");
       $("#a_image").addClass("in_display");
     } else if (current == 1) {
-      $(".in_display").addClass("non_display");
-      $("#i_image").removeClass("non_display");
       $(".in_display").removeClass("in_display");
       $("#i_image").addClass("in_display");
     } else if (current == 2) {
-      $(".in_display").addClass("non_display");
-      $("#u_image").removeClass("non_display");
       $(".in_display").removeClass("in_display");
       $("#u_image").addClass("in_display");
     } else if (current == 3) {
-      $(".in_display").addClass("non_display");
-      $("#e_image").removeClass("non_display");
       $(".in_display").removeClass("in_display");
       $("#e_image").addClass("in_display");
     } else if (current == 4) {
-      $(".in_display").addClass("non_display");
-      $("#o_image").removeClass("non_display");
       $(".in_display").removeClass("in_display");
       $("#o_image").addClass("in_display");
    } else if (current == 5 || current == 6) {
-      $(".in_display").addClass("non_display");
-      $("#n_image").removeClass("non_display");
       $(".in_display").removeClass("in_display");
       $("#n_image").addClass("in_display");
    }
